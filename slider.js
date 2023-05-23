c6befb77b15cac2f1c7181f8e3f2c9e7fc2bd0f8
Ekaterina Moldavskaya <kama115@mail.ru>
2023-05-23 23:38:09 +0200

    Change design of carousel element to make it the same as other workshops

--- slider.js
+++ slider.js
@@ -1,32 +1,61 @@
 fetch("workshops1.json")
   .then((response) => response.json())
   .then((json) => {
-    let swiperWrapper = document.querySelector(".swiper-wrapper");
-
-    for (i = 0; i < json.length; i++) {
+        for (i = 0; i < json.length; i++) {
       if (json[i].isPopular === true) {
-        let swiperSlide = document.createElement("div");
-        swiperSlide.className = "swiper-slide";
-        swiperSlide.innerHTML = `
-            <div class='slides'>
-        
-        <div>
-          <p class="title">${json[i].title}</p>
-          <br />
-          <i class="material-icons" style="font-size: 25px; padding-left: 10px">
-            signal_cellular_alt_2_bar
-          </i>
+         let swiperSlide = document.createElement("div");
+         swiperSlide.className = "swiper-slide";
+         swiperSlide.innerHTML =`<div class="box" onclick="popupForWorkshop()">
+         <div class="box2" style="background-color: #999999"><img class="imageOfType" src="${json[i].typeImage}" width="30px"></div>
+         <div class="box3">
+           <p class="title">${json[i].title}</p>
+           <br />
+           <i class="material-icons" style="font-size: 25px; padding-left: 10px">
+             signal_cellular_alt_2_bar
+           </i>
+ 
+           <span class="icon-span">${json[i].difficulty}</span>
+           <i class="material-icons">group</i>
+           <span class="icon-span">${json[i].minNumberOfParticipants} - ${json[i].maxNumberOfParticipants}</span>
+           <i class="material-icons">schedule</i>
+           <span class="icon-span">${json[i].minDuration} - ${json[i].maxDuration}</span>
+         </div>`;
+         let swiperWrapper = document.querySelector(".swiper-wrapper");
+         swiperWrapper.appendChild(swiperSlide);
+  }
+}
+  
 
-          <span class="icon-span">${json[i].difficulty}</span>
-          <i class="material-icons">group</i>
-          <span class="icon-span">${json[i].minNumberOfParticipants} - ${json[i].maxNumberOfParticipants}</span>
-          <i class="material-icons">schedule</i>
-          <span class="icon-span">${json[i].minDuration} - ${json[i].maxDuration}</span>
-        </div>
-            `;
-        swiperWrapper.appendChild(swiperSlide);
-      }
-    }
+
+// fetch("workshops1.json")
+//   .then((response) => response.json())
+//   .then((json) => {
+//     let swiperWrapper = document.querySelector(".swiper-wrapper");
+
+//     for (i = 0; i < json.length; i++) {
+//       if (json[i].isPopular === true) {
+//         let swiperSlide = document.createElement("div");
+//         swiperSlide.className = "swiper-slide";
+//         swiperSlide.innerHTML = `
+//             <div class='slides'>
+        
+//         <div>
+//           <p class="title">${json[i].title}</p>
+//           <br />
+//           <i class="material-icons" style="font-size: 25px; padding-left: 10px">
+//             signal_cellular_alt_2_bar
+//           </i>
+
+//           <span class="icon-span">${json[i].difficulty}</span>
+//           <i class="material-icons">group</i>
+//           <span class="icon-span">${json[i].minNumberOfParticipants} - ${json[i].maxNumberOfParticipants}</span>
+//           <i class="material-icons">schedule</i>
+//           <span class="icon-span">${json[i].minDuration} - ${json[i].maxDuration}</span>
+//         </div>
+//             `;
+//         swiperWrapper.appendChild(swiperSlide);
+//       }
+//     }
     const swiper = new Swiper(".swiper", {
       // Optional parameters
       direction: "horizontal",
