9c8206914c3f44a9d18a762ad666547e2fee057a
Ekaterina Moldavskaya <kama115@mail.ru>
2023-05-24 23:36:46 +0200

    add load more button and add additional workshops

--- script.js
+++ script.js
@@ -1,41 +1,98 @@
-//add new workshops from json file dinamically
-fetch("workshops1.json")
-  .then((response) => response.json())
-  .then((json) => {
-    for (let i = 0; i < json.length; i++) {
+//add first 9 workshops dinamically from json file when the page is loaded
+fetch('workshops1.json').then((response) => response.json()).then((json) => {
+  elementQuantity = json.length;
+  console.log(elementQuantity);
+  let workshopsToDisplay;
+  if (elementQuantity < 9) {
+    workshopsToDisplay = elementQuantity;
+  } else {
+    workshopsToDisplay = 9;
+  }
+  for (let i = 0; i < workshopsToDisplay; i++) {
+    const modal = document.createElement("div");
+    modal.classList.add('modal');
+
+    modal.innerHTML = `<div class="box" onclick="popupForWorkshop()">
+    <div class="box2" style="background-color: ${json[i].backgroundColor}"><img class="imageOfType" src="${json[i].typeImage}" width="30px"></div>
+    <div class="box3">
+    <div class="workshops-title">
+      <p class="title">${json[i].title}</p>
+      </dev>
+      <br />
+      <div class="icons">
+      <div class="info-icon">
+      <i class="material-icons" style="font-size: 25px; padding-left: 10px">
+        signal_cellular_alt_2_bar
+      </i>
+      
+      <span class="icon-span">${json[i].difficulty}</span>
+      </div>
+      <div class="info-icon">
+      <i class="material-icons">group</i>
+      <span class="icon-span">${json[i].minNumberOfParticipants} - ${json[i].maxNumberOfParticipants}</span>
+      </div>
+      <div class="info-icon">
+      <i class="material-icons">schedule</i>
+      <span class="icon-span">${json[i].minDuration} - ${json[i].maxDuration}</span>
+      </div>
+      </div>
+    </div>`;
+    const container = document.getElementById("container");
+    container.appendChild(modal);
+    modal.id = i;
+  }
+});
+
+//add new portion of workshops when user clicks "Load more" button
+function loadMoreWorkshops() {
+  fetch('workshops1.json').then((response) => response.json()).then((json) => {
+    const workshopListAtPage = document.getElementsByClassName('modal');
+    let numberOfWorkshops = workshopListAtPage.length;
+    console.log(`Workshops at the page: ${numberOfWorkshops}`);
+    let workshopsToAdd;
+    console.log(`Workshops left: ${json.length - numberOfWorkshops}`);
+    if (json.length - numberOfWorkshops < 9) {
+      workshopsToAdd = json.length - numberOfWorkshops;
+    } else {
+      workshopsToAdd = 9;
+    }
+    for (let i = numberOfWorkshops; i < numberOfWorkshops + workshopsToAdd; i++) {
       const modal = document.createElement("div");
-      modal.classList.add("modal");
+      modal.classList.add('modal');
 
       modal.innerHTML = `<div class="box" onclick="popupForWorkshop()">
-          <div class="box2" style="background-color: ${json[i].backgroundColor}"><img class="imageOfType" src="${json[i].typeImage}" width="30px"></div>
-          <div class="box3">
-          <div class="workshops-title">
-            <p class="title">${json[i].title}</p>
-            </dev>
-            <br />
-            <div class="icons">
-            <div class="info-icon">
-            <i class="material-icons" style="font-size: 25px; padding-left: 10px">
-              signal_cellular_alt_2_bar
-            </i>
-            
-            <span class="icon-span">${json[i].difficulty}</span>
-            </div>
-            <div class="info-icon">
-            <i class="material-icons">group</i>
-            <span class="icon-span">${json[i].minNumberOfParticipants} - ${json[i].maxNumberOfParticipants}</span>
-            </div>
-            <div class="info-icon">
-            <i class="material-icons">schedule</i>
-            <span class="icon-span">${json[i].minDuration} - ${json[i].maxDuration}</span>
-            </div>
-            </div>
-          </div>`;
+      <div class="box2" style="background-color: ${json[i].backgroundColor}"><img class="imageOfType" src="${json[i].typeImage}" width="30px"></div>
+      <div class="box3">
+      <div class="workshops-title">
+        <p class="title">${json[i].title}</p>
+        </dev>
+        <br />
+        <div class="icons">
+        <div class="info-icon">
+        <i class="material-icons" style="font-size: 25px; padding-left: 10px">
+          signal_cellular_alt_2_bar
+        </i>
+        
+        <span class="icon-span">${json[i].difficulty}</span>
+        </div>
+        <div class="info-icon">
+        <i class="material-icons">group</i>
+        <span class="icon-span">${json[i].minNumberOfParticipants} - ${json[i].maxNumberOfParticipants}</span>
+        </div>
+        <div class="info-icon">
+        <i class="material-icons">schedule</i>
+        <span class="icon-span">${json[i].minDuration} - ${json[i].maxDuration}</span>
+        </div>
+        </div>
+      </div>`;
       const container = document.getElementById("container");
       container.appendChild(modal);
       modal.id = i;
     }
   });
+}
+
+
 
 //popup opens when user clicks on workshops
 function popupForWorkshop() {
@@ -60,6 +117,7 @@ function popupForWorkshop() {
           document
             .getElementById("popup-main-image")
             .setAttribute("src", json[elementId].popupImage);
+            document.getElementById("textNumber").innerText = json[elementId].popupText;
           document.getElementById("myPopup").classList.toggle("show");
         };
       }
